45edf12ee4964e9c29fc814826287fb0e5d3ac27
RenHec <emisor.tarea@gmail.com>
2023-05-24 11:24:45 -0600

    Ejemplo de llave foranea

--- BaseDatos/Tablas/CREATE TABLE [dbo].[PRODUCTO].sql
+++ BaseDatos/Tablas/CREATE TABLE [dbo].[PRODUCTO].sql
@@ -1,17 +1,20 @@
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[PRODUCTO]') AND type in (N'U'))
 BEGIN
 	CREATE TABLE [dbo].[PRODUCTO](
-		[Id] [decimal](18, 0) IDENTITY(1,1) NOT NULL,				
+		[Id] [decimal](18, 0) IDENTITY(1,1) NOT NULL,	
+		[CategoriaId] [decimal](18, 0) NOT NULL,	
 		[Nombre] [nvarchar](75) NOT NULL,	
-		[Tipo] [nvarchar](75) NOT NULL,
-		[Precio] [Decimal](20,2) NOT NULL,	
-		[Vencimiento] [datetime] NOT NULL,			
+		[PrecioCosto] [decimal](20,2) NOT NULL,	
+		[PrecioVenta] [decimal](20,2) NOT NULL,	
+		[Vencimiento] [datetime] NULL,			
 	 CONSTRAINT [PK_PRODUCTO] PRIMARY KEY CLUSTERED	
 	(
 		[Id] DESC
 	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 80) ON [PRIMARY]
 	) ON [PRIMARY]
 	
+	ALTER TABLE [dbo].[PRODUCTO]  WITH CHECK ADD  CONSTRAINT [FK_PRODUCTO_CategoriaId] FOREIGN KEY([CategoriaId])
+	REFERENCES [dbo].[CATEGORIA] ([Id])
 
 	PRINT N'CREATE TABLE [dbo].[PRODUCTO]'
 END
